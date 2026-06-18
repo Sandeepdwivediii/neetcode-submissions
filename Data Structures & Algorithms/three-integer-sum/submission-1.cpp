933c598a5164f25ef936a4d066a3dd17ad939195
sandeep kumar <172915658+Sandeepdwivediii@users.noreply.github.com>
2026-06-18 23:01:19 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n-2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i];
+            int l = i+1, r = n - 1;
+             int sum = nums[l] + nums[r];
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum == target) {
+                ans.push_back({nums[i], nums[l], nums[r]});
+                l++;
+                r--;
+                
+                while(l < r && nums[l] == nums[l-1]) l++;
+                while(l < r && nums[r] == nums[r+1]) r--;
+                }
+           
+           else if(sum > target) r--;
+           else if(sum < target) l++;
+            }
+        }
+        return ans;
+    }
+};
